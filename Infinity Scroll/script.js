6b2640189774122e7a87607620ebdfbe333a2bdc
Catalin Maeschi <catalin.maeschi23@gmail.com>
2022-09-26 14:27:33 +0300

    Fetching Data

---
+++ Infinity Scroll/script.js
@@ -0,0 +1,19 @@
+
+// Unsplash API
+const count = 10;
+const apiKey = 'oOoAhycxcCp1POBjSLWUKptsrONDha0YXZ2mMovZSKE';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
